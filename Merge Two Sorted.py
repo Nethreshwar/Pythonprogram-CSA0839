223e101b72a6356540510e6d6acc801db36757a2
Nethreshwar <112603835+Nethreshwar@users.noreply.github.com>
2022-09-24 09:29:59 +0530

    Update Merge Two Sorted.py

--- Merge Two Sorted.py
+++ Merge Two Sorted.py
@@ -1,6 +1,5 @@
 l1 = []
 l2 = []
-
 print("Enter element for list1: ")
 while True:
   s = input()
@@ -8,7 +7,6 @@ while True:
     break
   else:
     l1.append(int(s))
-
 print("Enter element for list2: ")
 while True:
   s = input()
@@ -16,7 +14,6 @@ while True:
     break
   else:
     l2.append(int(s))
-
 l1.extend(l2)
 l1.sort()
 print(l1)
