b1e3ee89ece30ec03bdd87b559870db4a0c91157
Nethreshwar <112603835+Nethreshwar@users.noreply.github.com>
2022-09-24 09:27:50 +0530

    Update Max no.of words.py

--- Max no.of words.py
+++ Max no.of words.py
@@ -1,10 +1,10 @@
-x=[]
+n=[]
 count=0
 a=int(input("Enter number of string :"))
 for i in range(0,a):
       s=input("Sentence =")
       n.append(s)
-for j in x:
+for j in n:
       s=j.split()
       count = max(count,len(s))
 print(count)
